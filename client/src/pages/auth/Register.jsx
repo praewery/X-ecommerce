3a44f129887f5a1e7ea2197247d6c6f6afa8d17d
Praewa Choobanna <cytokinin8@gmail.com>
2025-02-02 15:12:55 +0700

    create register

--- client/src/pages/auth/Register.jsx
+++ client/src/pages/auth/Register.jsx
@@ -1,21 +1,51 @@
-import React from 'react'
+import React ,{ useState }from 'react'
 
 const Register = () => {
+
+  const [form,setForm] = useState({
+    email:"",
+    password:"",
+    confirmPassword:""
+  })
+  const handleOnChange = (e)=>{
+    // console.log(e.target.name,e.target.value)
+    setForm({
+      ...form,
+      [e.target.name]: e.target.value
+    })
+  }
+  const handleSubmit = (e)=>{
+    e.preventDefault()
+    console.log(form)
+  }
+
   return (
     <div>
         Register
-        <form>
+        <form onSubmit={ handleSubmit }>
 
             Email
             <input className='border'
+            onChange={handleOnChange}
             name='email'
             type='email'
             />
             Password
             <input className='border'
+            onChange={handleOnChange}
             name='password'
             type='text'
             />
+            Confirm Password 
+            <input className='border'
+            onChange={handleOnChange}
+            name='confirmPassword'
+            type='text'
+            />
+            <buttuon className='bg-blue-500 rounded-md'>
+              Register
+            </buttuon>
+
             
         </form>
 
